1ba54e9abfc4f6ec5d948bbe5f784af9cbc0d23f
Timur Makimov <timur.makimov@cae.com>
2023-10-24 16:25:12 -0700

    added SignalR project

---
+++ SignalRServer/Program.cs
@@ -0,0 +1,21 @@
+using SignalRServer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors();
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.UseCors(builder => builder
+    .WithOrigins("null")
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials()
+);
+
+app.MapHub<ChatHub>("/chatHub");
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
